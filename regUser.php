262d68b47acfb0fa8e00a0365d05901cc8f3568f
alvardsoler <alsoler@ucm.es>
2016-05-05 12:20:42 +0200

    added regUser function. doesnt work

--- regUser.php
+++ regUser.php
@@ -1,10 +1,57 @@
 <?php
-function regUser($username, $pass, $email, $showemail){
-	echo '$username $pass $email $showemail';
+function conectar($host, $usuario, $pass=''){
+	$db = @mysqli_connect($host, $usuario, $pass);
+	if ($db){
+		echo 'Conexión realizada con exito. <br />';
+		echo 'Información sobre el servidor: ', mysqli_get_host_info($db), '<br/>';
+		echo 'Versión del servidor: ', mysqli_get_server_info($db), '<br/>';		
+	}else{
+		printf('Error %d: %s.<br/>', mysqli_connect_errno(), mysqli_connect_error());
+		echo 'Error <br/>';
+	}
+	return $db;
 }
-	echo '$_POST[\'nombre\'] -> ',$_POST['username'], '<br \>';
-	echo '$_POST[\'pass\'] -> ',$_POST['pass'], '<br \>';
-	echo '$_POST[\'email\'] -> ',$_POST['email'], '<br \>';
-	echo '$_POST[\'showemail\'] -> ',$_POST['showemail'], '<br \>';
-	echo '$_REQUEST[\'nombre\'] -> ',$_REQUEST['username'], '<br \>';
+
+function desconectar($conexion){
+	if ($conexion){
+		$ok = @mysqli_close($conexion);
+		if ($ok){
+			echo 'Desconexión realizada correctamente. <br />';			
+		}else {
+			echo 'Fallo en la desconexión. <br />';
+		}
+	}else {
+		echo 'Conexión no abierta. <br />';
+	}
+}
+function viewUsers($db){
+	$sql = 'SELECT * FROM users';
+	$consulta = mysqli_query($db, $sql);
+	echo "<strong> lista de usuarios </strong>";
+	if (mysqli_fetch_assoc($consulta))
+		while ($fila = mysqli_fetch_assoc($consulta)){
+			echo $fila['username'], ' - ' , $fila['email'] , '<br/>';
+		}
+	else
+		echo "nada en la bd<br />";
+}
+
+function regUser($db, $username, $pass, $email, $showemail = false){
+	echo 'registring user: ', $username,' ', $pass, ' ', $email, ' ', $showemail, "<br/>";
+	$query = "INSERT INTO users(username, email, password, show_email) 
+		VALUES('$username', '$email', '$pass', '$showemail')";
+	echo "querying: ", $query, "<br/>";
+	//$consulta = mysqli_query($db, $query);
+	mysqli_query($db, $query) or trigger_error(mysqli_error($query));
+}
+
+	$db = conectar('localhost', 'root');
+	
+	if ($db) {
+
+		regUser($db, $_POST['username'], $_POST['pass'], $_POST['email'], true);
+
+		viewUsers($db);
+		desconectar($db);
+	}	
 ?>
